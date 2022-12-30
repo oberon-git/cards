c6a9311a805553b5516325553271cb166254650f
oberon-uofa <alexanderecooper@arizona.edu>
2022-12-30 13:41:26 -0700

    commit

--- src/shared/net.py
+++ src/shared/net.py
@@ -2,17 +2,6 @@ import pickle
 from .game import Game
 
 
-def send_int(conn, i):
-    b = str.encode(str(i))
-    conn.sendall(b)
-
-
-def recv_int(conn):
-    b = conn.recv(2)
-    print(b)
-    return int(b.decode())
-
-
 def send_str(conn, s):
     conn.sendall(str.encode(s))
 
@@ -31,21 +20,11 @@ def recv_initial_game(conn):
     return game_dict['p'], Game(game_dict['deck'], game_dict['players'])
 
 
-def send_game(conn, game):
-    data = pickle.dumps(game)
-    conn.sendall(data)
-
-
-def recv_game(conn):
-    data = conn.recv(2048*16)
-    return pickle.loads(data)
-
-
 def send_packet(conn, packet):
     data = pickle.dumps(packet)
     conn.sendall(data)
 
 
 def recv_packet(conn):
-    packet = pickle.loads(conn.recv(2048*4))
+    packet = pickle.loads(conn.recv(2048*16))
     return packet
