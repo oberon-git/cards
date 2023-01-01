b83362a86f3a42cbc8bed59d69cf52cfa57fe015
oberon-uofa <alexanderecooper@arizona.edu>
2023-01-01 15:09:25 -0700

    commit

--- start_cards.sh
+++ start_cards.sh
@@ -2,5 +2,5 @@
 
 git stash
 git pull
-export PYTHONPATH=${PYTHONPATH}:$./src
-python3 ~/Documents/Alexander/cards/src/client.py > /dev/nul
+export PYTHONPATH=${PYTHONPATH}:$~/Documents/Alexander/cards/src
+python3 ~/Documents/Alexander/cards/src/client.py
